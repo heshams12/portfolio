caf1ed1f8779a2f31d5b1338fa3b98634c99e188
hesham sobh <Heshams12@hotmail.com>
2018-08-23 14:29:34 -0700

    updated server.js

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use(bodyParser.json());
 // Static directory to be served
 app.use(express.static("app/public"));
 
-// Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
+
 
 // Starts the server to begin listening
 // =============================================================
